43e94c895eab67e25cbc5960c9e01b650c31f0fc
Hamidrezaf-dev <hr.fazlolahi@gmail.com>
2021-04-22 19:29:56 -0600

    node.js ex 5

---
+++ index4.js
@@ -0,0 +1,12 @@
+var firstname = "H";
+var lastname = "F";
+var age = 37;
+var address = "8373 2839 Ave";
+var dateofbirth = "8/26/1990";
+
+
+console.log("First name : ",firstname);
+console.log("Last name : ",lastname);
+console.log("Date of birth:",dateofbirth);
+console.log("Age : ",age);
+console.log("Address",address);
